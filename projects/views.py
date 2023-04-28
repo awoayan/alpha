663f854184d4730f6ff4896570b22423c878b9d1
Ayan <foxtrotsin9@gmail.com>
2023-04-28 12:06:38 -0400

    Feature 15 complete

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_list_or_404
+from django.shortcuts import render, redirect
 from projects.models import Project
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -14,3 +14,28 @@ def list_projects(request):
         "projects": projects,
     }
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def show_project(request, id):
+    project = Project.objects.get(id=id)
+    context = {
+        "project": project,
+    }
+    return render(request, "projects/detail.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(False)
+            project.owner = request.user
+            project.save()
+            return redirect("list_projects")
+
+    else:
+        form = ProjectForm()
+    context = {"form": form}
+    return render(request, "projects/create.html", context)
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from tasks.models import Task
+from tasks.forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required
+def create_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list_projects")
+    else:
+        form = TaskForm()
+    return render(request, "tasks/create.html", {"form": form})
+
+
+@login_required
+def show_my_tasks(request):
+    tasks = Task.objects.filter(assignee=request.user)
+    context = {
+        "tasks": tasks,
+    }
+    return render(request, "tasks/mine.html", context)
